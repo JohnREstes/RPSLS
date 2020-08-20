1585e5ef41b0aeede1c844040717acf1d41379be
John R. Estes <John.R.Estes@icloud.com>
2020-08-20 19:27:21 -0400

    added whoWho array

--- RPSLS.js
+++ RPSLS.js
@@ -1,12 +1,10 @@
 "use strict";
 
-Game.mainMenu();
-
 class Player {
 
     constructor(name){
         this.name = name;
-        this.choice = ['rock', 'paper', 'scissor', 'lizard', 'Spock'];
+        this.choice = ['rock ', 'paper ', 'scissor ', 'lizard ', 'Spock '];
         this.turn = '';
         this.win = [];
     } 
@@ -14,33 +12,44 @@ class Player {
      console.log("The Player is named " + this.name + ".");
     }
     playTurn(){
-        this.turn = prompt("Would you like to choose:\n" + this.choice).toLowerCase;
+        return this.turn = prompt("Would you like to choose:\n" + this.choice).toLowerCase;
     }
 }
 
 class Human extends Player{
     constructor(name){
-        super(name, choice, turn, win)
+        super(name)
     }
 }
 class Computer extends Player{
     constructor(name){
-        super(name, choice, turn, win)
+        super(name)
     }
     playTurn(){
         let choice = Math.floor(Math.random() * 5);
-        this.turn = this.choice[choice];
+        return this.turn = this.choice[choice];
     }
 }
 
 class Game {
-
+    let whoWonArray = {
+        ['Rock', 'crushes', 'Scissors'],
+        ['Rock', 'crushes', 'Lizard'],
+        ['Paper', 'covers', 'Rock'],
+        ['Paper', 'disproves', 'Spock'],
+        ['Scissors', 'cuts', 'Paper'],
+        ['Scissors', 'decapitates', 'Lizard'],
+        ['Lizard', 'poisons', 'Spock'],
+        ['Lizard', 'eats', 'Paper'],
+        ['Spock', 'smashes', 'Scissors'],
+        ['Spock', 'vaporizes', 'Rock']
+    }
     constructor(){
         this.playerOne = null;
         this.playerTwo = null;
     }
 
-    chooseNumberOfPlayers(){
+    chooseNumberOfPlayers(userInput){
         if(userInput == "one"){
             this.playerOne = new Human();
             this.playerTwo = new Computer();
@@ -49,8 +58,6 @@ class Game {
             this.playerOne = new Human();
             this.playerTwo = new Human();           
         }
-        this.playerOne.playTurn();
-        this.playerTwo.playTurn();
     }
 
     mainMenu(){
@@ -59,11 +66,11 @@ class Game {
             promptResponse = prompt("Welcome to Rock, Paper, Scissor, Lizard, Spock!\n\nWould you like to play best of three or see the rules?\n\nType the option you want: 'play' 'rules' or 'quit'");
             switch(promptResponse){
                 case 'play':
-                    Game.howManyPlayers();
-                    Game.gamePlay();
+                    this.howManyPlayers();
+                    this.gamePlay();
                     break;
                 case 'rules':
-                    Game.rules();
+                    this.rules();
                     break;
                 case 'quit':
                     break;
@@ -73,11 +80,13 @@ class Game {
         }
         while(promptResponse !== 'quit')
     }
+
     rules(){
         alert("This is a twist on Rock Paper Scissors!\n\nIn this version, you can choose from:\n\nRock, Paper, Scissor, Lizard or Spock.");
         alert("These are the possible combinatins:\n\nRock crushes Scissors and Rock crushes Lizard\nPaper covers Rock and Paper disproves Spock\nScissors cuts Paper and Scissors decapitates Lizard\nLizard poisons Spock and Lizard eats Paper\nSpock smashes Scissors and Spock vaporizes Rock");
         alert("You will also be able to choose whether you would like to play against the computer or play against a friend.\n\nGood Luck!");
     } 
+
     howManyPlayers(){
         let promptResponse = "";   
         do{
@@ -85,7 +94,7 @@ class Game {
             switch(promptResponse){
                 case 'one':
                 case 'two':
-                    Game.chooseNumberOfPlayers(promptResponse);
+                    this.chooseNumberOfPlayers(promptResponse);
                     
                     break;
                 case 'quit':
@@ -96,9 +105,16 @@ class Game {
         }
         while(promptResponse !== 'quit')    
     }
+
     gamePlay(){
+        let playerOneTurn = this.playerOne.playTurn();
+        let playerTwoTurn = this.playerTwo.playTurn();
+        this.whoWon(playerOneTurn, playerTwoTurn);
 
-        let choice = prompt("What would you like your move to be?\n\nRock, Paper, Scissor, Lizard or Spock?");
+    }  
+    whoWon(playerOneTurn, playerTwoTurn){
 
-    }   
+    } 
 }
+let gamePlay = new Game();
+gamePlay.mainMenu();
