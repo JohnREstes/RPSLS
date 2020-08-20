0eba88dea2c8973b3033a22f404a81490a1786b2
John R. Estes <John.R.Estes@icloud.com>
2020-08-20 19:00:46 -0400

    added game class, moved functions into class

--- RPSLS.js
+++ RPSLS.js
@@ -1,6 +1,6 @@
 "use strict";
 
-mainMenu();
+Game.mainMenu();
 
 class Player {
 
@@ -18,17 +18,12 @@ class Player {
     }
 }
 
-class PlayerOne extends Player{
+class Human extends Player{
     constructor(name){
         super(name, choice, turn, win)
     }
 }
-class PlayerTwo extends Player{
-    constructor(name){
-        super(name, choice, turn, win)
-    }
-}
-class computer extends Player{
+class Computer extends Player{
     constructor(name){
         super(name, choice, turn, win)
     }
@@ -38,50 +33,72 @@ class computer extends Player{
     }
 }
 
-function mainMenu(){
-    let promptResponse = "";   
-    do{
-        promptResponse = prompt("Welcome to Rock, Paper, Scissor, Lizard, Spock!\n\nWould you like to play best of three or see the rules?\n\nType the option you want: 'play' 'rules' or 'quit'");
-        switch(promptResponse){
-            case 'play':
-                
-                break;
-            case 'rules':
-                rules();
-                break;
-            case 'quit':
-                break;
-            default:
-                alert("That is an invald response");
-        }
+class Game {
+
+    constructor(){
+        this.playerOne = null;
+        this.playerTwo = null;
     }
-    while(promptResponse !== 'quit')
-}
-function rules(){
-    alert("This is a twist on Rock Paper Scissors!\n\nIn this version, you can choose from:\n\nRock, Paper, Scissor, Lizard or Spock.");
-    alert("These are the possible combinatins:\n\nRock crushes Scissors and Rock crushes Lizard\nPaper covers Rock and Paper disproves Spock\nScissors cuts Paper and Scissors decapitates Lizard\nLizard poisons Spock and Lizard eats Paper\nSpock smashes Scissors and Spock vaporizes Rock");
-    alert("You will also be able to choose whether you would like to play against the computer or play against a friend.\n\nGood Luck!");
-} 
-function howManyPlayers(){
-    let promptResponse = "";   
-    do{
-        promptResponse = prompt("How Many Players?\n\nType the option you want: 'one' 'two' or 'quit'");
-        switch(promptResponse){
-            case 'one':
-            
-                break;
-            case 'two':
-            
-                break;
-            case 'quit':
-                break;
-            default:
-                alert("That is an invald response");
+
+    chooseNumberOfPlayers(){
+        if(userInput == "one"){
+            this.playerOne = new Human();
+            this.playerTwo = new Computer();
         }
+        else{
+            this.playerOne = new Human();
+            this.playerTwo = new Human();           
+        }
+        this.playerOne.playTurn();
+        this.playerTwo.playTurn();
     }
-    while(promptResponse !== 'quit')    
+
+    mainMenu(){
+        let promptResponse = "";   
+        do{
+            promptResponse = prompt("Welcome to Rock, Paper, Scissor, Lizard, Spock!\n\nWould you like to play best of three or see the rules?\n\nType the option you want: 'play' 'rules' or 'quit'");
+            switch(promptResponse){
+                case 'play':
+                    Game.howManyPlayers();
+                    Game.gamePlay();
+                    break;
+                case 'rules':
+                    Game.rules();
+                    break;
+                case 'quit':
+                    break;
+                default:
+                    alert("That is an invald response");
+            }
+        }
+        while(promptResponse !== 'quit')
+    }
+    rules(){
+        alert("This is a twist on Rock Paper Scissors!\n\nIn this version, you can choose from:\n\nRock, Paper, Scissor, Lizard or Spock.");
+        alert("These are the possible combinatins:\n\nRock crushes Scissors and Rock crushes Lizard\nPaper covers Rock and Paper disproves Spock\nScissors cuts Paper and Scissors decapitates Lizard\nLizard poisons Spock and Lizard eats Paper\nSpock smashes Scissors and Spock vaporizes Rock");
+        alert("You will also be able to choose whether you would like to play against the computer or play against a friend.\n\nGood Luck!");
+    } 
+    howManyPlayers(){
+        let promptResponse = "";   
+        do{
+            promptResponse = prompt("How Many Players?\n\nType the option you want: 'one' 'two' or 'quit'");
+            switch(promptResponse){
+                case 'one':
+                case 'two':
+                    Game.chooseNumberOfPlayers(promptResponse);
+                    
+                    break;
+                case 'quit':
+                    break;
+                default:
+                    alert("That is an invald response");
+            }
+        }
+        while(promptResponse !== 'quit')    
+    }
+    gamePlay(){
+
+        let choice = prompt("What would you like your move to be?\n\nRock, Paper, Scissor, Lizard or Spock?");
+
+    }   
 }
-function gamePlay(){
-    let choice = prompt("What would you like your move to be?\n\nRock, Paper, Scissor, Lizard or Spock?");
-    
-}   
